f2b841e138b1282aa20a209a9536b384faf73a7a
Roman Dodin <dodin.roman@gmail.com>
2018-02-21 14:54:50 +0300

    added script to provision dockerfiles

---
+++ add_dockerfile.sh
@@ -0,0 +1,27 @@
+NUAGE_VERSION="$1"
+VSPK_VERSION="$2"
+
+[[ -z "$NUAGE_VERSION" ]] && { echo "Error: Nuage version must be passed as a first argument"; exit 1; }
+
+[[ -z "$VSPK_VERSION" ]] && { VSPK_VERSION=$NUAGE_VERSION; }
+
+CATS_DEP_PACKAGES="future jinja2 py2-ipaddress pyvmomi PyYAML robotframework robotframework-sshlibrary tabulate vspk==$VSPK_VERSION"
+
+git checkout -b $NUAGE_VERSION
+
+cat << EOF > Dockerfile
+FROM alpine:3.6
+
+RUN apk add --no-cache python py-paramiko && \\
+    python -m ensurepip && \\
+    rm -r /usr/lib/python*/ensurepip && \\
+    pip install --upgrade pip setuptools && \\
+    pip install $CATS_DEP_PACKAGES && \\
+    rm -r /root/.cache
+
+ENV NUAGE_RELEASE $NUAGE_VERSION
+EOF
+
+git add Dockerfile
+git commit -m "added $NUAGE_VERSION"
+git push origin $NUAGE_VERSION
