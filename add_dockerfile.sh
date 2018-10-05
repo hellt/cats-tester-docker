491bb6345e5909d427f13f9f92348a47932ccfa6
Anatoly Kolpakov <feroxsnake@me.com>
2018-10-05 17:42:52 +0300

    Update add_dockerfile.sh

--- add_dockerfile.sh
+++ add_dockerfile.sh
@@ -12,10 +12,11 @@ git checkout -b $NUAGE_VERSION
 cat << EOF > Dockerfile
 FROM alpine:3.6
 
-RUN apk add --no-cache python py-paramiko && \\
+RUN apk add --no-cache python py-paramiko build-base python2-dev openssl-dev && \\
     python -m ensurepip && \\
     rm -r /usr/lib/python*/ensurepip && \\
     pip install --upgrade pip setuptools && \\
+    pip install -U six && \\
     pip install $CATS_DEP_PACKAGES && \\
     rm -r /root/.cache
 
